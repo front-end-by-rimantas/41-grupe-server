d666dbe44383f9eb88072dd0ebcf93abf1fa1e65
Rimantas <belauzas@yahoo.com>
2023-01-25 15:06:22 +0200

    Update

--- config.js
+++ config.js
@@ -3,11 +3,13 @@ const config = {};
 config.dev = {
     name: 'dev',
     port: 41069,
+    cache: 0,
 }
 
 config.prod = {
     name: 'prod',
     port: 41070,
+    cache: 86400,
 }
 
 const envName = process.env.NODE_ENV;
