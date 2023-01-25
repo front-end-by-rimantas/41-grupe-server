c08cdbc1781c2851b2415dda84b7d8ec57c165b9
Rimantas <belauzas@yahoo.com>
2023-01-25 13:47:22 +0200

    Update

--- pages/home.js
+++ pages/home.js
@@ -2,7 +2,8 @@ import { PageTemplate } from "../lib/PageTemplate.js";
 
 class PageHome extends PageTemplate {
     mainHTML() {
-        return `<h1>Welcome to HOME PAGE!</h1>`;
+        return `<h1>Welcome to HOME PAGE!</h1>
+                <img src="/img/yoga.png" alt="Yoga">`;
     }
 }
 
